db91757d6d61dc24dbe5e8daf04fba1b137c3fb9
Ashutosh Sharma <ashutoshsharma86416@gmail.com>
2023-04-30 17:52:00 +0530

    initial commit

---
+++ default.js
@@ -0,0 +1,32 @@
+module.exports={
+    "logging":false,
+    "port":3400,
+    "requestLimit":"100000000",
+    "sendGridKey":"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",
+    "emailSender":"IPTSE",
+    "dbUri":"mongodb+srv://technologydefault:o5jneZWRz2SpdqJX@iptse.ckveaif.mongodb.net/api?retryWrites=true&w=majority",
+    "awsConfig":{
+        "endpoint":"sadughi",
+        "accessKeyId":"sduhfsaui",
+        "secretAccessKey":"sduhfsaui",
+    },
+    "isProduction":true,
+    "s3Bucket":"iptsebucket.s3.ap-south-1.amazonaws.com/files",
+    "maxFileSize":"50M",
+    "awsSES":"AKIAQEMRDC7YGIGV3MII EqeSWmXY4B2X7YwDkwaTmFEOSindVzCEiPHRHsh5",
+    "isTesting":false,
+    "QNVERT_URL":"http://localhost:4200",
+    "fcmKey":"weoijr9q8rh879qwhr8iwhq8",
+    "pagination":10,
+    "razorPayKey":"sdidfds",
+    "razorPaySecret":"dlfkjsdiohfiuh",
+    "version":"v1",
+    "allowedImagesTypes":"png,jpg,jpeg",
+    "allowedVideoTypes":"mp4",
+    "jwtSecret":"",
+    "webLink":"",
+    "tokenLength":"",
+    "allowedFileTypes":"",
+    "swaggerHost":"sduihfiouesgh"
+
+}
